b5fffad6582bb689a569524b04312f0670d55043
evoluir <contato@evoluirinformatica.com.br>
2014-02-03 16:00:42 -0200

    nova alteracao 005/14

--- qualimaster/res_company.py
+++ qualimaster/res_company.py
@@ -10,6 +10,7 @@ class res_company(osv.osv):
     _inherit = 'res.company'
     _columns = {
                 'porcent_comiss': fields.float('% Comissão', digits=(6,4)),
+                'gestor_id': fields.many2one('hr.employee', 'Gestor da Empresa', domain="[('is_tech_mananger','=',True)]",),
                 }
 
 res_company()
